2266d7b6e6ff266e2d950c52612a5a8eea3fdef7
ValenYukel <vyukelson@udesa.edu.ar>
2024-11-11 12:11:16 -0300

    arrgelo config

--- myapp/database/config/config.js
+++ myapp/database/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   "development": {
     "username": "root",
     "password": "root",
-    "database": NOMBRE-A-CREAR,
+    "database": "NOMBRE-A-CREAR",
     "host": "127.0.0.1",
     "dialect": "mysql"
   }
