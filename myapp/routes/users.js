800c6cf3b70a3435f477fda7a8db925acbecaf7a
freijomil <mfreijomil@udesa.edu.ar>
2024-11-14 14:28:18 -0300

    progreso en login y register

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
+const userController = require("../controllers/userControllers");
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+router.get('/login', userController.login);
+
+router.get('/register', userController.register);
 
 module.exports = router;
