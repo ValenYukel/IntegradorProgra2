bb77fc1bfdd6c0733f18d3c92f336c9cc8a2e3ed
ValenYukel <vyukelson@udesa.edu.ar>
2024-11-14 13:40:07 -0300

    cambios carpeta css

--- myapp/app.js
+++ myapp/app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var catalogoRouter = require('./routes/catalogo');
+
 
 var app = express();
 
@@ -21,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalogo', catalogoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
