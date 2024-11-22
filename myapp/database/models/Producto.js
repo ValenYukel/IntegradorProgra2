67cf7f7b2a12823eff66288f5429eca4f5159f9d
BenjaOrchansky <borchansky@udesa.edu.ar>
2024-11-22 18:17:43 -0300

    relaciones modelos

--- myapp/database/models/Producto.js
+++ myapp/database/models/Producto.js
@@ -31,5 +31,12 @@ module.exports = (sequelize, DataTypes) => {
 
     const Producto = sequelize.define(alias,cols,config);
     
+    Producto.associate = function(models) {
+        Producto.belongsTo(models.Usuario, {
+            as: "usuario",
+            foreignKey: "usuario_id"
+        });
+    }
     return Producto;
 }
+
